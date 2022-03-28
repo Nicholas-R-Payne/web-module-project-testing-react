c82767fbc64733407b2e3f362b33f54649e28812
DarkedgeTH <darkedgeth@gmail.com>
2022-03-28 15:15:22 -0400

    completed 4th Show test

--- src/components/tests/Show.test.js
+++ src/components/tests/Show.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { render, fireEvent, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Show from './../Show';
+import userEvent from '@testing-library/user-event';
 
 const testShow = {
     name: 'test show',
@@ -36,6 +37,12 @@ test('renders same number of options seasons are passed in', () => {
     expect(seasonOptions).toHaveLength(2)
 });
 
-test('handleSelect is called when an season is selected', () => { });
+test('handleSelect is called when an season is selected', () => {
+    const handleSelect = jest.fn()
+    render(<Show show={testShow} selectedSeason={'none'} handleSelect={handleSelect}/>)
+    const select = screen.getByLabelText(/Select A Season/i)
+    userEvent.selectOptions(select, ['1'])
+    expect(handleSelect).toBeCalled()
+});
 
 test('component renders when no seasons are selected and when rerenders with a season passed in', () => { });
