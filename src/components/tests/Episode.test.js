b0d537eb20461eea58e7e3ba0303f261d2b1d091
DarkedgeTH <darkedgeth@gmail.com>
2022-03-28 14:15:13 -0400

    set up project and completed 1st test

--- src/components/tests/Episode.test.js
+++ src/components/tests/Episode.test.js
@@ -3,8 +3,18 @@ import { render, fireEvent, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Episode from './../Episode';
 
-test("renders without error", () => { });
+const testEpisode = {
+    id: 1,
+    name: '',
+    image: 'http://static.tvmaze.com/uploads/images/original_untouched/200/501942.jpg',
+    season: 1,
+    number: 1,
+    summary: '',
+    runtime: 1
+}
 
+test("renders without error", () => { });
+    render(<Episode episode={testEpisode} />)
 test("renders the summary test passed as prop", () => { });
 
 test("renders default image when image is not defined", () => { });
