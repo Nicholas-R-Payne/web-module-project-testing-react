4aab7fc0611f35395b35f0972a18e947baa4505d
DarkedgeTH <darkedgeth@gmail.com>
2022-03-28 15:36:17 -0400

    completed 4th Display test

--- src/components/tests/Display.test.js
+++ src/components/tests/Display.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, fireEvent, screen, waitFor } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Display from './../Display';
 import userEvent from '@testing-library/user-event';
@@ -46,3 +46,14 @@ test('renders show season options matching your data when the button is clicked'
         expect(seasonOptions).toHaveLength(2)
     })
 });
+
+test('displayFunc is called when the fetch button is pressed', async () => {
+    mockFetchShow.mockResolvedValueOnce(testShow)
+    const displayFunc = jest.fn()
+    render(<Display displayFunc={displayFunc} />)
+    const button = screen.getByRole('button')
+    userEvent.click(button)
+    await waitFor(() => {
+        expect(displayFunc).toHaveBeenCalled()
+    })
+})
