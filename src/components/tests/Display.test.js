2ca7ab11dc18f28870750bd696b590397753c78c
DarkedgeTH <darkedgeth@gmail.com>
2022-03-28 15:21:20 -0400

    completed 1st Display test

--- src/components/tests/Display.test.js
+++ src/components/tests/Display.test.js
@@ -4,7 +4,7 @@ import '@testing-library/jest-dom/extend-expect';
 import Display from './../Display';
 
 test('renders without errors with no props', async () => { });
-
+    render(<Display />)
 test('renders Show component when the button is clicked ', () => { });
 
 test('renders show season options matching your data when the button is clicked', () => { });
