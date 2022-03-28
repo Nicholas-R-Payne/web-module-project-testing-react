1ae308ee7d377414303d08674f60446ab29133cf
DarkedgeTH <darkedgeth@gmail.com>
2022-03-28 15:26:43 -0400

    completed 2nd Display test

--- src/components/tests/Display.test.js
+++ src/components/tests/Display.test.js
@@ -2,9 +2,35 @@ import React from 'react';
 import { render, fireEvent, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Display from './../Display';
+import userEvent from '@testing-library/user-event';
 
-test('renders without errors with no props', async () => { });
+const testShow = {
+    name: 'test show',
+    summary: 'test summary',
+    seasons: [
+        {
+            id: 0,
+            name: 'Season 1',
+            episodes: []
+        },
+        {
+            id: 1,
+            name: 'Season 2',
+            episodes: []
+        },
+    ]
+}
+
+test('renders without errors with no props', async () => {
     render(<Display />)
-test('renders Show component when the button is clicked ', () => { });
+});
+
+test('renders Show component when the button is clicked ', async () => {
+    render(<Display />)
+    const button = screen.getByRole('button')
+    userEvent.click(button)
+    const show = await screen.findByTestId('show-container')
+    expect(show).toBeInTheDocument()
+});
 
 test('renders show season options matching your data when the button is clicked', () => { });
